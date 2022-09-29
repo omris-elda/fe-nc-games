df114ae986784f4422264801bea639e2ba603c55
Ed <ed@wearenet.co.uk>
2022-09-29 13:55:48 +0100

    viewing comment on individual review page functionality

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,32 @@
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { LoggedIn } from "../../contexts/loggedin";
 
 export default function NavBar() {
+  const { loggedIn, setLoggedIn } = useContext(LoggedIn);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const logOut = () => {
+    setLoggedIn("");
+  };
 
-    return (
-      <nav>
-        <button className="buttons" onClick={() => navigate("/reviews")}>
-          Reviews
+  return (
+    <nav>
+      <button className="buttons" onClick={() => navigate("/reviews")}>
+        Reviews
+      </button>
+      <button className="buttons" onClick={() => navigate("/categories")}>
+        Categories
+      </button>
+      {loggedIn ? (
+        <button className="buttons" onClick={() => logOut()}>
+          Logout
         </button>
-        <button className="buttons" onClick={() => navigate("/categories")}>
-          Categories
-        </button>
+      ) : (
         <button className="buttons" onClick={() => navigate("/login")}>
           Login
         </button>
-      </nav>
-    );
+      )}
+    </nav>
+  );
 }
